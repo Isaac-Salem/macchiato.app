1d6356e53f1d6aec0470227346bf7223542cfe22
Isaac Salem <67123265+Isaac-Salem@users.noreply.github.com>
2025-04-19 22:11:12 -0500

    Make app base route deployment dependant
    vite.config.js checks for `process.env.GITHUB_ACTIONS`
    to set the app base as '/macchiato,app/' when creating
    a build in a github workflow. It also checks for a user
    defined env var 'CUST_BASE_URL', which if set, will use
    it's contents as the base instead.

--- client/src/main.js
+++ client/src/main.js
@@ -22,8 +22,7 @@ const routes = [
 ]
 
 const router = VueRouter.createRouter({
-    // history: VueRouter.createWebHistory(process.env.GITHUB_PAGES ? '/macchiato.app/' : '/'),
-    history: VueRouter.createWebHistory('/macchiato.app/'),
+    history: VueRouter.createWebHistory(import.meta.env.BASE_URL),
     scrollBehavior() {return { top: 0 }},
     routes
 })
--- client/vite.config.js
+++ client/vite.config.js
@@ -5,11 +5,21 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), ''); // Load .env files
+
+    let url_base = '/';
+    if (process.env.GITHUB_ACTIONS)
+    {
+      url_base = '/macchiato.app/';
+    }
+    else if (process.env.CUST_BASE_URL)
+    {
+      // if set, assign the base name to a custom name from CUST_BASE_URL env variable
+      url_base = process.env.CUST_BASE_URL;
+    }
+
     return {
       plugins: [vue()],
-      // base: env.GITHUB_PAGES ? '/macchiato.app/' : '/',
-      base: '/macchiato.app/',
+      base: url_base,
       resolve: {
         alias: {
           '@': fileURLToPath(new URL('/client/src', import.meta.url))
